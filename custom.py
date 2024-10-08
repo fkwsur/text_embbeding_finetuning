28f529c50d42ee821a8f9ba68825f3ecdab427e2
Hyun-Ji <guswl543210@gmail.com>
2024-10-08 15:42:04 +0900

    통신

--- custom.py
+++ custom.py
@@ -1,8 +1,15 @@
 from sentence_transformers import SentenceTransformer
+from flask import Flask, request, jsonify
 
-# 내모델돌렵보기
-model = SentenceTransformer('./hyunji_embbeding', trust_remote_code=True)
+app = Flask(__name__)
 
-embd = model.encode('hello, fine')
+@app.route('/embedding', methods=['POST'])
+def process_embedding():
+    data = request.json
+    model = SentenceTransformer('./hyunji_embbeding', trust_remote_code=True)
+    embd = model.encode(data['data'])
+    result = {"embedding": embd.tolist()}  # 임시 결과
+    return jsonify(result)
 
-print(embd)
+if __name__ == '__main__':
+        app.run(port=5000)
